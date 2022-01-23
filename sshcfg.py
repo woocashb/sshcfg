c5f280d285a11c3d1c418a6472549a7aaca5eb1e
Łukasz Boczkaja <lboczkaja@unity-t.pl>
2022-01-23 12:14:50 +0100

    added ssh entry regex and did minor refactoring

--- sshcfg.py
+++ sshcfg.py
@@ -3,22 +3,32 @@
 import jinja2
 import os
 import sys
+import re
+
 
 def usage():
     print("Add new entry to ssh config at default location for current user\nUsage: {} Host Hostname User [ProxyCommand]".format(sys.argv[0]))
+
 
 if not len(sys.argv) >= 4:
     print("Missing mandatory arguments!")
     usage()
     sys.exit(1)
 
-ssh_config_path = os.path.expanduser("~/.ssh/config2")
+ssh_config_path = os.path.expanduser("~/.ssh/config.dev")
 
-ssh_config_entry = """
+ssh_entry_options = "(hostname|proxycommand|user|port)"
+
+ssh_entry_regex = re.compile(\
+"Host (.*)\n \
+(\s*{0} .*)*".format(ssh_entry_options)
+, flags=re.I | re.M)
+
+ssh_entry_template = """
 Host {{ ssh_host }}
   HostName {{ ssh_host_ip }}
   User {{ ssh_user }}
-  {% if ssh_proxy_command -%}
+  {%- if ssh_proxy_command -%}
   ProxyCommand {{ ssh_proxy_command }}
   {%- endif -%}
 """
@@ -32,6 +42,6 @@ else:
   ssh_proxy_command_arg = None
 
 
-ssh_config_entry_template = jinja2.Template(ssh_config_entry)
+ssh_entry_instance = jinja2.Template(ssh_entry_template)
 with open(ssh_config_path, 'a') as ssh_config_fd:
-    ssh_config_fd.write(ssh_config_entry_template.render(ssh_host=ssh_host_arg, ssh_host_ip=ssh_host_ip_arg, ssh_user=ssh_user_arg, ssh_proxy_command=ssh_proxy_command_arg))
+    ssh_config_fd.write(ssh_entry_instance.render(ssh_host=ssh_host_arg, ssh_host_ip=ssh_host_ip_arg, ssh_user=ssh_user_arg, ssh_proxy_command=ssh_proxy_command_arg))
