cabdd3cd21d864f5b12c638fd1ba1322ed748d3c
Łukasz Boczkaja <lboczkaja@unity-t.pl>
2022-02-15 07:33:03 +0100

    added argument handling by argparse

--- sshcfg.py
+++ sshcfg.py
@@ -4,18 +4,10 @@ import jinja2
 import os
 import sys
 import re
+import argparse
+from pathlib import Path
 
-def usage():
-    print("Add new entry to ssh config at default location for current user\
-     \nUsage: {} Host Hostname User [ProxyCommand]".format(sys.argv[0]))
-
-
-if not len(sys.argv) >= 4:
-    print("Missing mandatory arguments!")
-    usage()
-    sys.exit(1)
-
-ssh_config_path = os.path.expanduser(".ssh/config.dev")
+ssh_config_path = Path("./.ssh/config.dev")
 
 ssh_entry_options = "(hostname|proxycommand|user|port)"
 
@@ -24,24 +16,27 @@ ssh_entry_regex = re.compile(\
 (\s*{0} .*)*".format(ssh_entry_options)
 , flags=re.I | re.M)
 
-ssh_entry_template = """
+ssh_entry_template = """\n
 Host {{ ssh_host }}
-  HostName {{ ssh_host_ip }}
+  HostName {{ ssh_hostname }}
   User {{ ssh_user }}
-  {%- if ssh_proxy_command -%}
+  {%- if ssh_proxy_command %}
   ProxyCommand {{ ssh_proxy_command }}
-  {%- endif -%}
+  {%- endif %}
 """
 
-ssh_host_arg = sys.argv[1]
-ssh_host_ip_arg = sys.argv[2]
-ssh_user_arg = sys.argv[3]
-if len(sys.argv) == 5:
-  ssh_proxy_command_arg = sys.argv[4]
-else:
-  ssh_proxy_command_arg = None
+parser = argparse.ArgumentParser(description="Manage ssh config entries")
+MANDATORY_ARGS = ('host', 'hostname', 'user', 'proxy_command')
+for mandatory_arg in MANDATORY_ARGS:
+    parser.add_argument(mandatory_arg)
+
+ssh_cfg_entry = parser.parse_args()
+
+ssh_cfg_entry.host = sys.argv[1]
+ssh_cfg_entry.hostname = sys.argv[2]
+ssh_cfg_entry.user = sys.argv[3]
 
 
 ssh_entry_instance = jinja2.Template(ssh_entry_template)
 with open(ssh_config_path, 'a') as ssh_config_fd:
-    ssh_config_fd.write(ssh_entry_instance.render(ssh_host=ssh_host_arg, ssh_host_ip=ssh_host_ip_arg, ssh_user=ssh_user_arg, ssh_proxy_command=ssh_proxy_command_arg))
+    ssh_config_fd.write(ssh_entry_instance.render(ssh_host=ssh_cfg_entry.host, ssh_hostname=ssh_cfg_entry.hostname, ssh_user=ssh_cfg_entry.user, ssh_proxy_command=ssh_cfg_entry.proxy_command))
