1f4b377e956e72f8cbfbe52e309b89f451f19166
Łukasz Boczkaja <lboczkaja@unity-t.pl>
2022-02-16 20:06:15 +0100

    added listing of ssh entries

--- sshcfg.py
+++ sshcfg.py
@@ -25,13 +25,41 @@ Host {{ ssh_host }}
   {%- endif -%}
 """
 
+
+def add_entry(add_args):
+    ssh_host, ssh_hostname, ssh_user = add_args.add
+    ssh_proxy_command = add_args.proxy_command
+    ssh_entry_instance = jinja2.Template(ssh_entry_template)
+    with open(ssh_config_path, 'a') as ssh_config_fd:
+        ssh_config_fd.write(ssh_entry_instance.render(ssh_host=ssh_host, ssh_hostname=ssh_hostname, ssh_user=ssh_user, ssh_proxy_command=ssh_proxy_command))
+
+def list_entries():
+    with open(ssh_config_path) as ssh_config_fd:
+        print(ssh_config_fd.read())
+
 parser = argparse.ArgumentParser(description="Manage ssh config entries")
 MANDATORY_ARGS = ('host', 'hostname', 'user')
-for mandatory_arg in MANDATORY_ARGS:
-    parser.add_argument(mandatory_arg)
-parser.add_argument('proxy_command', nargs='?', default=None)
-ssh_cfg_entry = parser.parse_args()
 
-ssh_entry_instance = jinja2.Template(ssh_entry_template)
-with open(ssh_config_path, 'a') as ssh_config_fd:
-    ssh_config_fd.write(ssh_entry_instance.render(ssh_host=ssh_cfg_entry.host, ssh_hostname=ssh_cfg_entry.hostname, ssh_user=ssh_cfg_entry.user, ssh_proxy_command=ssh_cfg_entry.proxy_command))
+parser.add_argument('-a', '--add', nargs=len(MANDATORY_ARGS),
+                    metavar=MANDATORY_ARGS,
+                    help='add new ssh config entry')
+
+parser.add_argument('proxy_command', nargs='?', default=None)
+parser.add_argument('-l', '--list', action="store_true", help='list ssh config entries')
+
+
+args = parser.parse_args()
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit()
+
+if args.add:
+  add_entry(args)
+
+if args.list:
+  list_entries()
+
+
+# if args.list:
+#     list_entries()
